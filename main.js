503612e1234cd76f762761bbeb8b97cafb20ee07
mateussiilva <mateuspython3010@gmail.com>
2024-06-04 21:27:13 -0300

    Create squelet the project

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// alert('Olá mundo')
+const containerImage = document.querySelector("#container-image")
+const inputTexto = document.querySelector("#inome").value
+const imagens = {
+    "black": "assets/imgs/black.jpg",
+    "green": "assets/imgs/green.jpg",
+    "orange": "assets/imgs/orange.jpg",
+    "red": "assets/imgs/red.jpg",
+    "yellow": "assets/imgs/yellow.jpg",
+}
+function mudarBackground(img) {
+    containerImage.src = imagens[img]
+    console.log(imagens.img)
+}
+function escreverNaImage() {
+    console.log(inputTexto)
+
+}
