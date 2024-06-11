cdb47b8baf59032dcf8b74553712cd1e8918e731
mateussiilva <mateuspython3010@gmail.com>
2024-06-11 20:30:39 -0300

    Container de minituras

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
-const containerImage = document.querySelector("#container-image")
-const inputTexto = document.querySelector("#inome").value
-const imagens = {
-    "black": "assets/imgs/black.jpg",
-    "green": "assets/imgs/green.jpg",
-    "orange": "assets/imgs/orange.jpg",
-    "red": "assets/imgs/red.jpg",
-    "yellow": "assets/imgs/yellow.jpg",
+// const containerImage = document.querySelector("#container-image")
+// const inputTexto = document.querySelector("#inome").value
+// const imagens = {
+//     "black": "assets/imgs/black.jpg",
+//     "green": "assets/imgs/green.jpg",
+//     "orange": "assets/imgs/orange.jpg",
+//     "red": "assets/imgs/red.jpg",
+//     "yellow": "assets/imgs/yellow.jpg",
+// }
+// function mudarBackground(img) {
+//     containerImage.src = imagens[img]
+//     console.log(imagens.img)
+// }
+// function escreverNaImage() {
+//     console.log(inputTexto)
+
+// }
+
+const containerBackgrounds = document.querySelector(".backgrounds")
+const backgrounds = {
+
+    img1: "assets/imgs/black.jpg",
+    img2: "assets/imgs/green.jpg",
+    img3: "assets/imgs/orange.jpg",
+    img4: "assets/imgs/red.jpg",
+
+
 }
-function mudarBackground(img) {
-    containerImage.src = imagens[img]
-    console.log(imagens.img)
+for (const imgBack in backgrounds) {
+    let containerMiniatura = document.createElement("div")
+    let pathImage = backgrounds[imgBack]
+    const miniatura = document.createElement("img")
+    miniatura.classList.add("miniatura")
+    miniatura.src = pathImage
+    miniatura.addEventListener("click", e => {
+        changeImage(pathImage)
+        // alert(e)
+    })
+    containerMiniatura.appendChild(miniatura)
+    containerBackgrounds.appendChild(containerMiniatura)
+    // containerBackgrounds.
 }
-function escreverNaImage() {
-    console.log(inputTexto)
+console.log(containerMiniatura)
+
+// const containerBackground = document.querySelector(".backgrounds")
+function changeImage(pathImage) {
+    let containerImage = document.querySelector("#imageToalha")
+    containerImage.src = pathImage
 
 }
