2980be02e5b11f8965f9231c0a8763d1374bde89
mateussiilva <mateuspython3010@gmail.com>
2024-06-11 22:08:47 -0300

    Update projeCT

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-// const containerImage = document.querySelector("#container-image")
-// const inputTexto = document.querySelector("#inome").value
-// const imagens = {
-//     "black": "assets/imgs/black.jpg",
-//     "green": "assets/imgs/green.jpg",
-//     "orange": "assets/imgs/orange.jpg",
-//     "red": "assets/imgs/red.jpg",
-//     "yellow": "assets/imgs/yellow.jpg",
-// }
-// function mudarBackground(img) {
-//     containerImage.src = imagens[img]
-//     console.log(imagens.img)
-// }
-// function escreverNaImage() {
-//     console.log(inputTexto)
 
-// }
 
 const containerBackgrounds = document.querySelector(".backgrounds")
 const backgrounds = {
